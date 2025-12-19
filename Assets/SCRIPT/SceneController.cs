c3d8299f23818537427590b60bc32a9b9dd08f04
rokutandaryu <ryu.rokutanda.3k@stu.hosei.ac.jp>
2025-12-19 14:59:24 +0900

    roku7

--- Assets/SCRIPT/SceneController.cs
+++ Assets/SCRIPT/SceneController.cs
@@ -1,16 +1,44 @@
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
+/// <summary>
+/// シーン遷移を管理するスクリプト
+/// </summary>
 public class SceneController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+
+    /// <summary>
+    /// 初期化処理
+    /// </summary>
+    private void Start()
     {
-        
+    
     }
 
-    // Update is called once per frame
-    void Update()
+    /// <summary>
+    /// 指定された名前のシーンに遷移する処理
+    /// </summary>
+    public void LoadScene(string targetSceneName)
     {
-        
+        // 指定されたシーン名のシーンに遷移
+        SceneManager.LoadScene(targetSceneName);
     }
+    
+    
+    /// <summary>
+    /// ゲームを終了する処理
+    /// </summary>
+    public void OnQuitButtonClick()
+    {
+        Debug.Log("ゲームを終了します");
+#if UNITY_EDITOR
+        // エディタの場合
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        // ビルドされたゲームの場合
+        Application.Quit();
+#endif
+    }
+
 }
